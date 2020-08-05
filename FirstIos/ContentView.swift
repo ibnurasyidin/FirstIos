d229d5fba4deb2daef5aa2b677289e4edd3ff2e1
trimuerisandes <trimuerisandes@gmail.com>
2020-08-05 14:09:48 +0700

    Initial Commit

---
+++ FirstIos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstIos
+//
+//  Created by Tri Mueri sandes on 05/08/20.
+//  Copyright © 2020 Tri Mueri sandes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
